8939b822827c3ef7f5df7c33b653f7298ceb43bd
himchawla <himchawla43@gmail.com>
2021-04-26 01:00:02 +1200

    ex

---
+++ Project1/Source.cpp
@@ -0,0 +1,29 @@
+
+#include<math.h>
+
+struct point
+{
+    float x;
+    float y;
+};
+
+float sign(point p1, point p2, point p3)
+{
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+bool PointInTriangle(point pt, point v1, point v2, point v3)
+{
+    float d1, d2, d3;
+    bool neg, pos;
+
+    d1 = sign(pt, v1, v2);
+    d2 = sign(pt, v2, v3);
+    d3 = sign(pt, v3, v1);
+
+    neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
+    pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+    return !(neg && pos);
+}
+
--- spaceInvaders/Source.cpp
+++ spaceInvaders/Source.cpp
@@ -6,54 +6,305 @@
 
 # define M_PI           3.14159265358979323846
 
+
+struct line
+{
+	float a;
+	float b;
+	float c;
+	float x1, x2, y1, y2;
+	bool intersected = false;
+	void calculate(sf::Vector2f p1, sf::Vector2f p2)
+	{
+		x1 = p1.x; x2 = p2.x; y1 = p1.y; y2 = p2.y;
+		float m = (p2.y - p1.y) / (p2.x - p1.x);
+		a = m;
+		b = -1.0f;
+		c = m * p1.x - p1.y;
+	}
+
+	sf::Vector2f intersectWith;
+	bool inBetween(sf::Vector2f _1, sf::Vector2f _2, sf::Vector2f t)
+	{
+		float x1, x2, y1, y2;
+
+		x1 = _1.x < _2.x ? _1.x : _2.x;
+		x2 = _1.x < _2.x ? _2.x : _1.x;
+
+		y1 = _1.y < _2.y ? _1.y : _2.y;
+		y2 = _1.y < _2.y ? _2.y : _1.y;
+
+		if (t.x > x1 && t.x < x2 && t.y > y1 && t.y < y2)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+
+
+	}
+
+	bool intersect(line l2, sf::Vector2f& result)
+	{
+		float determinant = a * l2.b - l2.a * b;
+
+		if (determinant == 0)
+		{
+
+			return false;
+		}
+
+		result.x = (c * l2.b - l2.c * b) / determinant;
+		result.y = (a * l2.c - l2.a * c) / determinant;
+		float x = result.x;
+		float y = result.y;
+
+		if (inBetween(sf::Vector2f(x1, y1), sf::Vector2f(x2, y2), result) && inBetween(sf::Vector2f(l2.x1, l2.y1), sf::Vector2f(l2.x2, l2.y2), result))
+			return true;
+		else
+		{
+			result.x = 19200.0f;
+			result.y = 19200.0f;
+			return false;
+		}
+	}
+};
+
 int main()
 {
+	bool lineSet = false;
+	bool triangleSet = false;
+
 	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML");
 
-	sprite* p1 = nullptr, *p2 = nullptr, *p3 = nullptr;
+	sprite* p1 = nullptr, * p2 = nullptr, * p3 = nullptr, * n1 = nullptr, * n2 = nullptr;
 	bool p11 = false, p22 = false, p33 = false;
-	sprite l1, l2, l3;
-	l1.setTexture("Assets/line.png");
-	l2.setTexture("Assets/line.png");
-	l3.setTexture("Assets/line.png");
-	
 
-	
+	sf::Vector2f* result1 = new sf::Vector2f(19200.0f, 19200.0f), * result2 = new sf::Vector2f(19200.0f, 19200.0f);
+
+	sf::ConvexShape convex;
+	sf::ConvexShape intersect;
+	sf::ConvexShape triangleShape[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		triangleShape[i].setPointCount(3);
+	}
+
+	line triangle[3];
+
+	line cut;
+
+	// resize it to 5 points
+	convex.setPointCount(3);
+	intersect.setPointCount(4);
+
+
 	//Game Loop
 	while (window.isOpen())
 	{
-		
+
 		window.setKeyRepeatEnabled(true);
 		sf::Mouse mouse;
 
-		if (p1 != nullptr)
+		window.clear();
+
+
+		// define the points
+		if (p1 != nullptr && p2 != nullptr && p3 != nullptr && !triangleSet)
 		{
-			p1->draw(window);
+			convex.setFillColor(sf::Color::Blue);
+			convex.setPoint(0, p1->sp.getPosition());
+			convex.setPoint(1, p2->sp.getPosition());
+			convex.setPoint(2, p3->sp.getPosition());
+			triangleSet = true;
+			triangle[0].calculate(convex.getPoint(0), convex.getPoint(1));
+			triangle[1].calculate(convex.getPoint(1), convex.getPoint(2));
+			triangle[2].calculate(convex.getPoint(2), convex.getPoint(0));
+
+
+		}
+		else if (!triangleSet)
+		{
+			if (p1 != nullptr)
+			{
+				p1->draw(window);
+			}
+
+			if (p2 != nullptr)
+			{
+				p2->draw(window);
+			}
+
+			if (p3 != nullptr)
+			{
+				p3->draw(window);
+			}
+
+
 		}
 
-		if (p22)
+		if (p1 != nullptr && p2 != nullptr && p3 != nullptr && n1 != nullptr && n2 != nullptr && !lineSet)
 		{
-			p2->draw(window);
+			lineSet = true;
+			intersect.setFillColor(sf::Color::Red);
+			intersect.setPoint(0, n1->sp.getPosition());
+			intersect.setPoint(1, n2->sp.getPosition());
+			intersect.setPoint(2, n2->sp.getPosition() + sf::Vector2f(0.0f, 0.0f));
+			intersect.setPoint(3, n1->sp.getPosition() + sf::Vector2f(0.0f, 0.0f));
+			intersect.setOutlineThickness(1.5f);
+			intersect.setOutlineColor(sf::Color::Red);
+
+			cut.calculate(intersect.getPoint(0), intersect.getPoint(1));
+
+
+			for (int i = 0; i < 3; i++)
+			{
+
+				if (*result1 == sf::Vector2f(19200.0f, 19200.0f) && triangle[i].intersect(cut, *result1))
+				{
+					std::cout << "int1";
+					triangle[i].intersected = true;
+					triangle[i].intersectWith = *result1;
+					continue;
+				}
+
+				else if (*result2 == sf::Vector2f(19200.0f, 19200.0f) && triangle[i].intersect(cut, *result2))
+				{
+					triangle[i].intersected = true;
+					triangle[i].intersectWith = *result2;
+
+					std::cout << "int 2";
+				}
+			}
+
+			if (*result1 != sf::Vector2f(19200.0f, 19200.0f) && *result2 != sf::Vector2f(19200.0f, 19200.0f))
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					if (triangle[i].intersected == false)
+					{
+						if (i == 1 || i == 0)
+						{
+							line temp = triangle[i];
+							triangle[i] = triangle[2];
+							triangle[2] = temp;
+						}
+
+					}
+				}
+
+				if (triangle[0].x1 == triangle[1].x2 && triangle[0].y1 == triangle[1].y2)
+				{
+					float tx = triangle[1].x1;
+					triangle[1].x1 = triangle[1].x2;
+					triangle[1].x2 = tx;
+
+					float ty = triangle[1].y1;
+					triangle[1].y1 = triangle[1].y2;
+					triangle[1].y2 = ty;
+
+				}
+
+				else if (triangle[0].x2 == triangle[1].x1 && triangle[0].y2 == triangle[1].y1)
+				{
+					float tx = triangle[0].x1;
+					triangle[0].x1 = triangle[0].x2;
+					triangle[0].x2 = tx;
+
+					float ty = triangle[0].y1;
+					triangle[0].y1 = triangle[0].y2;
+					triangle[0].y2 = ty;
+
+				}
+
+				else if (triangle[0].x2 == triangle[1].x2 && triangle[0].y2 == triangle[1].y2)
+				{
+					float tx = triangle[0].x1;
+					triangle[0].x1 = triangle[0].x2;
+					triangle[0].x2 = tx;
+
+					float ty = triangle[0].y1;
+					triangle[0].y1 = triangle[0].y2;
+					triangle[0].y2 = ty;
+
+					tx = triangle[1].x1;
+					triangle[1].x1 = triangle[1].x2;
+					triangle[1].x2 = tx;
+
+					ty = triangle[1].y1;
+					triangle[1].y1 = triangle[1].y2;
+					triangle[1].y2 = ty;
+
+				}
+				triangleShape[0].setFillColor(sf::Color::Red);
+				triangleShape[0].setPoint(0, sf::Vector2f(triangle[0].x1, triangle[0].y1));
+				triangleShape[0].setPoint(1, *result1);
+				triangleShape[0].setPoint(2, *result2);
+
+				triangleShape[1].setFillColor(sf::Color::Blue);
+				triangleShape[1].setPoint(0, sf::Vector2f(triangle[0].x2, triangle[0].y2));
+				triangleShape[1].setPoint(1, *result1);
+				triangleShape[1].setPoint(2, *result2);
+
+				triangleShape[2].setFillColor(sf::Color::Green);
+				triangleShape[2].setPoint(0, sf::Vector2f(triangle[0].x2, triangle[0].y2));
+				triangleShape[2].setPoint(1, sf::Vector2f(triangle[1].x2, triangle[1].y2));
+				triangleShape[2].setPoint(2, triangle[1].intersectWith);
+
+			}
 		}
 
-		if (p33)
+		else if (!lineSet)
 		{
-			p3->draw(window);
+			if (n1 != nullptr)
+			{
+				n1->draw(window);
+			}
+			if (n2 != nullptr)
+			{
+				n2->draw(window);
+			}
 		}
 
-		if (p11 && p22 && p33)
-		{
-			float dist = sqrt((p1->sp.getPosition().x -  p2->sp.getPosition().x) * (p1->sp.getPosition().x - p2->sp.getPosition().x) + (p1->sp.getPosition().y - p2->sp.getPosition().y)* (p1->sp.getPosition().y - p2->sp.getPosition().y));
-			float angle = std::atan((p1->sp.getPosition().y - p2->sp.getPosition().y) / (p1->sp.getPosition().x - p2->sp.getPosition().y)) * (180 / M_PI);
-			l1.setLocation(p1->sp.getPosition().x, p1->sp.getPosition().y);
-			//l1.sp.setOrigin(200, 0);
-			l1.sp.setRotation(angle + 180);
+		window.draw(intersect);
 
-			//l1.sp.setScale(dist, 1);
-			p11 = false;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::T))
+		{
+			/*p1 = nullptr;
+			p2 = nullptr;
+			p3 = nullptr;
+			n1 = nullptr;
+			n2 = nullptr;
+			triangleSet = false;
+
+			for (int i = 0; i < 3; i++)
+			{
+				triangleShape[i].setPointCount(0);
+				triangleShape[i].setPointCount(3);
+			}
+			result1->x = 19200.0f;
+			result1->y = 19200.0f;
+
+			result2->x = 19200.0f;
+			result2->y = 19200.0f;
+
+			lineSet = false;*/
+			window.close();
+			main();
 		}
-		l1.draw(window);
-		
+		if (triangleSet)
+		{
+			window.draw(convex);
+
+			for (int i = 0; i < 3; i++)
+			{
+				window.draw(triangleShape[i]);
+			}
+		}
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
@@ -64,9 +315,9 @@ int main()
 					p11 = true;
 					p1 = new sprite(mouse.getPosition(window).x, mouse.getPosition(window).y);
 					p1->setTexture("Assets/Point.png");
-				
+
 				}
-				else if (!p22)
+				else if (p2 == nullptr)
 				{
 					p22 = true;
 					p2 = new sprite(mouse.getPosition(window).x, mouse.getPosition(window).y);
@@ -74,20 +325,32 @@ int main()
 
 				}
 
-				else if (!p33)
+				else if (p3 == nullptr)
 				{
 					p33 = true;
 					p3 = new sprite(mouse.getPosition(window).x, mouse.getPosition(window).y);
 					p3->setTexture("Assets/Point.png");
 
 				}
+
+				else if (n1 == nullptr)
+				{
+					n1 = new sprite(mouse.getPosition(window).x, mouse.getPosition(window).y);
+					n1->setTexture("Assets/Point.png");
+				}
+
+				else if (n2 == nullptr)
+				{
+					n2 = new sprite(mouse.getPosition(window).x, mouse.getPosition(window).y);
+					n2->setTexture("Assets/Point.png");
+				}
 			}
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
-		
-	window.display();
+
+		window.display();
 	}
 }
 
